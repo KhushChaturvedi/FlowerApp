6894a0c4ab0a7e3b8dc2f5f84b58498dfd28abf0
Khush <chaturvedikhush816@gmail.com>
2026-07-20 21:45:17 +0530

    Initial Commit

---
+++ FlowerApp/FlowerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerAppApp.swift
+//  FlowerApp
+//
+//  Created by Khush  on 20/07/26.
+//
+
+import SwiftUI
+
+@main
+struct FlowerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
